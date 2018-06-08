7aaacfc252bd549ac4b86c0f477202db2b9c393c
missaoni <39814512+missaoni@users.noreply.github.com>
2018-06-08 08:47:41 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -8,3 +8,13 @@
 function shift(){
 	window.location="index.html";
 }
+
+window.onscroll=function()
+{ 
+    var t=document.documentElement.scrollTop||document.body.scrollTop;  
+    var f1=document.getElementById("function"); 
+    if(t>= 100){ 
+        f1.className = "function_1";
+    }else{
+        f1.className = "function";
+    }
