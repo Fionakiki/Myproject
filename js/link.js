8f5fc225fd9eb37cd829bc3ec6c5e2c372fd9ac8
Fionakiki <38949229+Fionakiki@users.noreply.github.com>
2018-06-08 11:21:35 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -5,16 +5,26 @@
  * @version $Id$
  */
 
-function shift(){
+function home(){
 	window.location="index.html";
+}
+function about(){
+	window.location="index_about.html";
+}
+function edit(){
+	window.location="index_edit.html";
+}
+function contact(){
+	window.location="index_contact.html";
 }
 
 window.onscroll=function()
 { 
     var t=document.documentElement.scrollTop||document.body.scrollTop;  
     var f1=document.getElementById("function"); 
-    if(t>= 100){ 
+    if(t>= 210){ 
         f1.className = "function_1";
     }else{
         f1.className = "function";
-    }
+    } 
+}
