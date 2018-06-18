903b375bbd324d68c9fb0c1c1a06710adb80d302
missaoni <39814512+missaoni@users.noreply.github.com>
2018-06-18 18:55:22 +0800

    Update link_part.js

--- js/link_part.js
+++ js/link_part.js
@@ -6,16 +6,16 @@
  */
 
 function home(){
-	window.location="../index.html";
+    window.location="../index.html";
 }
 function about(){
-	window.location="./index_about.html";
+    window.location="./index_about.html";
 }
 function edit(){
-	window.location="./index_edit.html";
+    window.location="./index_edit.html";
 }
 function contact(){
-	window.location="./index_contact.html";
+    window.location="./index_contact.html";
 }
 
 window.onscroll=function()
@@ -44,21 +44,58 @@ $('input[type=file]').change(function() {
 
 
 function style1(){
-	var sty1=document.getElementById("images");
-	if(jQuery("input[type='file']").val()==""){
-		alert("请上传照片");
+    var sty1=document.getElementById("images");
+    if(jQuery("input[type='file']").val()==""){
+        alert("请上传照片");
     }
     else{
-    	sty1.className="double-border";
+        sty1.className="double-border";
     }
 }
 
 function style2(){
-	var sty2=document.getElementById("images");
-	if(jQuery("input[type='file']").val()==""){
-		sty2.className="picture";
+    var sty2=document.getElementById("images");
+    if(jQuery("input[type='file']").val()==""){
+        sty2.className="picture";
     }
     else{
-    	sty2.className="outset-border";
+        sty2.className="outset-border";
     }
 }
+
+
+function editImage() {
+    
+    var gs       = $("#gs").val();      // grayscale
+    var blur     = $("#blur").val();    // blur
+    var br       = $("#br").val();      // brightness
+    var ct       = $("#ct").val();      // contrast
+    var huer     = $("#huer").val();    //hue-rotate
+    var opacity      = $("#opacity").val(); //opacity
+    var invert   = $("#invert").val();  //invert
+    var saturate     = $("#saturate").val();//saturate
+    var sepia    = $("#sepia").val();   //sepia
+
+    var filter =    'grayscale(' + gs+
+            '%) blur(' + blur +
+            'px) brightness(' + br +
+            '%) contrast(' + ct +
+            '%) hue-rotate(' + huer +
+            'deg) opacity(' + opacity +
+            '%) invert(' + invert +
+            '%) saturate(' + saturate +
+            '%) sepia(' + sepia + '%)';
+
+    $("#images").css("filter", filter);
+    $("#images").css("-webkit-filter", filter); 
+
+}
+
+$("input[type=range]").change(editImage).mousemove(editImage);
+
+
+$('#imageEditor').on('reset', function () {
+    setTimeout(function() {
+        editImage();
+    },0);
+});
